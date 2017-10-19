6a74a3dcdae0f05f9e19012586be4d115d6412e9
jpleva91 <jpleva91@gmail.com>
2017-10-19 14:04:06 -0600

    working on passport

--- prototypes/app.js
+++
@@ -1,9 +0,0 @@
-console.log("js loaded");
-
-$(document).ready(function() {
-    console.log("jQuery ready!");
-    $('.dropdown-toggle').on('click', function() {
-    	console.log("works");
-    }).dropdown("toggle");
-});
-
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 app.listen(3000, function() {
 	console.log("Go to localhost 3000");
