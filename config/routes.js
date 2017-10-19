8a8c0b43f4e2b78cc7d3fc8d8e77a45e88a0abd3
jpleva91 <jpleva91@gmail.com>
2017-10-19 14:38:58 -0600

    updates routes

--- config/routes.js
+++ config/routes.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const router = express.Router();
+// Parses information from POST
+const bodyParser = require('body-parser');
+// Used to manipulate POST methods
+const methodOverride = require('method-override');
+const passport = require('passport');
+const usersController = require('../controllers/users');
+const staticsController = require('../controllers/statics');
+
+router.route('/')
+	.get(staticsController.home);
+
+router.route('/signup')
+	.get(usersController.getSignup)
+	.post(usersController.postSignup);
+
+router.route('/login')
+	.get(usersController.getLogin)
+	.post(usersController.postLogin);
+
+router.route('/logout')
+	.get(usersController.getLogout);
+
+module.exports = router;
