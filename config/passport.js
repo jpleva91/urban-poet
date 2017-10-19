5c3c3571dbc57755d2f01d9df0d7c05e2bad8237
jpleva91 <jpleva91@gmail.com>
2017-10-19 14:34:09 -0600

    working on passport

---
+++ config/passport.js
@@ -0,0 +1,56 @@
+let LocalStrategy = require('passport-local').Strategy;
+let User = require('../models/user');
+
+module.exports = function(passport) {
+
+	passport.serializeUser(function(user, callback) {
+		callback(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, callback) {
+		User.findById(id, function(err, user) {
+			callback(err, user);
+		});
+	});
+
+	passport.use('local-signup', new LocalStrategy({
+		usernameField : 'email',
+		passwordField : 'password',
+		passReqToCallback : true
+	}, function(req, email, password, callback) {
+		//Find a user with this email
+		User.findOne({'local.email' : email}, function(err, user) {
+			if (err) return callback(err);
+			//If there already is a user with that email
+			if (user) {
+				return callback(null, false, req.flash('signupMessage', 'Username is already taken.'));
+			} else {
+				//There is not already a user with that email
+				//Create one
+				let newUser = new User();
+				newUser.local.email = email;
+				newUser.local.password = newUser. encrypt(password);
+				newUser.save(function(err) {
+					if (err) throw err;
+					return callback(null, newUser);
+				});
+			}
+		});
+	}));
+
+	passport.use('local-login', new LocalStrategy({
+		usernameField : 'email',
+		passwordField : 'password',
+		passReqToCallback : true
+	}, function(req, email, password, callback) {
+		User.findOne({'local.email' : email}, function(err, user) {
+			if (err) return callback(err);
+			//No user found
+			if(!user || !user.validPassword(password)) {
+				return callback(null, false, req.flash('loginMessage', 'Username or Password Incorrect'));
+			}
+
+			return callback(null, user);
+		});
+	}));
+};
--- server.js
+++ server.js
@@ -8,6 +8,33 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
+mongoose.connect('mongodb://localhost/urban-poet-authentication');
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser);
+
+app.set('views', './views');
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'URBAN-POET'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
+
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	next();
+});
+
+const routes = require('./config/routes');
+app.use(routes);
+
 app.listen(3000, function() {
 	console.log("Go to localhost 3000");
 });
