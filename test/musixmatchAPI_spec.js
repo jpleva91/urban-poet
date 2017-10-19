604429c7c2f34e0aa2898fe06a63479ec9406acc
jpleva91 <jpleva91@gmail.com>
2017-10-18 20:52:55 -0600

    updates tests

--- test/musixmatchAPI_spec.js
+++ test/musixmatchAPI_spec.js
@@ -2,14 +2,14 @@ const expect = require('chai').expect;
 const request = require('request');
 const apikey = require('../config/env.js');
 
-let url = "http://api.musixmatch.com/ws/1.1/track.search?q_artist=gza&q_track=shadowboxing&apikey="+apikey;
-
-// console.log("URL: ",url);
+let searchURL = "http://api.musixmatch.com/ws/1.1/track.search?q_artist=gza&q_track=shadowboxin'&apikey="+apikey;
+let trackId = "53410090";
+let lyricsURL = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id="+trackId+"&apikey="+apikey;
 
 describe("Search", function() {
-	var apiError, apiResponse, apiBody;
+	let apiError, apiResponse, apiBody;
 	before(function(done) {
-		request(url, function(error, response, body) {
+		request(searchURL, function(error, response, body) {
 			apiError = error;
 			apiResponse = response;
 			apiBody = body;
@@ -20,11 +20,34 @@ describe("Search", function() {
 		expect(apiResponse.statusCode).to.eq(200);
 	});
 
-	it("should have a sentence in the body", function() {
+	it("should return the track_id", function() {
 		if (typeof(apiBody) == "string") {
 			apiBody = JSON.parse(apiBody);
 		}
-		console.log(apiBody.message.body.track_list);
+		console.log(apiBody.message.body.track_list[0].track.track_id);
+		expect(apiBody).to.not.be.empty;
+	});
+});
+
+describe("Lyrics", function() {
+	let apiError, apiResponse, apiBody;
+	before(function(done) {
+		request(lyricsURL, function(error, response, body) {
+			apiError = error;
+			apiResponse = response;
+			apiBody = body;
+			done();
+		});
+	});
+	it("should return 200 - OK", function() {
+		expect(apiResponse.statusCode).to.eq(200);
+	});
+
+	it("should return the lyrics", function() {
+		if (typeof(apiBody) == "string") {
+			apiBody = JSON.parse(apiBody);
+		}
+		console.log(apiBody.message.body.lyrics.lyrics_body);
 		expect(apiBody).to.not.be.empty;
 	});
 });
