7a31488f1935d5290d782cbc7cdcf446ecfd31a3
jpleva91 <jpleva91@gmail.com>
2017-10-22 16:13:41 -0600

    updates add song page

--- public/scripts/addSong.js
+++ public/scripts/addSong.js
@@ -3,14 +3,17 @@ console.log("JavaScript loaded");
 $(document).ready(function() {
 	console.log("jQuery ready");
 
+	let inputTitle;
+	let inputArtist;
+	let inputSoundCloud;
+	let selectedLyricsId;
+
 	$('#searchLyrics').on('click', function(e) {
 		e.preventDefault();
 		console.log("Clickity");
-		let inputTitle = $('#inputTitle').val();		
-		let inputArtist = $('#inputArtist').val();
-		let inputSoundCloud = $('#inputSoundCloud').val();
-		console.log("title:", inputTitle);
-		console.log("artist:", inputArtist);
+		inputTitle = $('#inputTitle').val();		
+		inputArtist = $('#inputArtist').val();
+		inputSoundCloud = $('#inputSoundCloud').val();
 
 		if(!inputTitle || !inputArtist || !inputSoundCloud) {
 			alert("Please Fill Out All Fields");
@@ -26,20 +29,31 @@ $(document).ready(function() {
 			.done(function(data) {
 				let results = data.results;
 				$('#choose-lyrics').empty();
+				selectedLyricsId = false;
 				results.forEach(function(data) {
-					console.log("Song Id:", data.track_id);
-					console.log("Song Title:", data.title);
-					console.log("Song Artist:", data.artist);
 					$('#choose-lyrics').append('<button class="btn btn-warning col-md-12 playlist" id="' + data.track_id + '"><a class="song-button" href="#">' + data.title + ' by ' + data.artist + '</a></button>');
-					$('#choose-lyrics').on('click', 'button', function() {
-						console.log($(this).attr('id'));
-					});
 				});
 			});
-
 			$('#songModal').modal();
 		}
 	});
 
-	
+	$('#choose-lyrics').on('click', 'button', function() {
+			selectedLyricsId = $(this).attr('id');
+	});	
+
+	$('#save-song-button').on('click', function() {
+		if(!selectedLyricsId) {
+			alert("Please select lyrics from list");
+		}
+		else {
+			console.log("title:", inputTitle);
+			console.log("artist:", inputArtist);
+			console.log("soundcloud url:", inputSoundCloud);
+			console.log("selectedLyricsId:", selectedLyricsId);
+			$('#songModal').modal('hide');
+		}
+	});
+
+
 });
